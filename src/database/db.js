62fa57244791c8cb6ef7aefb390e78585250be74
Math <math@pop-os.localdomain>
2022-10-17 13:24:47 -0300

    arrumando a rota delete

--- src/database/db.js
+++ src/database/db.js
@@ -4,19 +4,11 @@ dotenv.config();
 
 const { Pool } = psql;
 
-/* const databaseConfig = {
+const databaseConfig = {
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
-} */
-
-const databaseConfig = {
-    user: 'postgres',
-    password: "582460",
-    host: 'localhost',
-    port: '5432',
-    database: 'shortly',
 }
 
 const connection = new Pool(databaseConfig);
